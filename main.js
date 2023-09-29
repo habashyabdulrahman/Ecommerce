30d11eeeb3577f28541376391057e0d70c6072a0
Habashy Abdulrahman <133090767+habashyabdulrahman@users.noreply.github.com>
2023-09-30 01:23:23 +0200

    e

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ let close = document.getElementById("close");
 let opne = document.querySelector("bar");
 let nav = document.getElementById("navbar");
 
-// if (bar) {
-//     bar.addEventListener("click", () => {
-//     nav.classList.add("active");
-//     });
-// }
-
 if (open){
     bar.addEventListener("click", () => {
     nav.classList.add("active");
@@ -18,4 +12,21 @@ if (close) {
     close.addEventListener("click", () => {
     nav.classList.remove("active");
     });
+}
+
+// Navigate between images
+let MainImg = document.getElementById("MainImg");
+let Smallimg = document.getElementsByClassName("small-img");
+
+Smallimg[0].onclick = function(){
+    MainImg.src = Smallimg[0].src;
+}
+Smallimg[1].onclick = function(){
+    MainImg.src = Smallimg[1].src;
+}
+Smallimg[2].onclick = function(){
+    MainImg.src = Smallimg[2].src;
+}
+Smallimg[3].onclick = function(){
+    MainImg.src = Smallimg[3].src;
 }
